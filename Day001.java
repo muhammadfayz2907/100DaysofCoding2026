2d6d630ca9b8f9c99f4bd2099d194e2bb72a7cad
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-02 21:16:10 +0800

    Day1
    Cihuyy

--- Day001.java
+++ Day001.java
@@ -1,7 +1,7 @@
 public class Day1{
     public static void main(String[] args) {
         /* Teks panjanggggggggggg */
-
+        /** cihuy */
         // komentar satu barisss
 
         System.out.println("Haripertamadalampercodingan");
