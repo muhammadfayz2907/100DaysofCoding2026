583a04097bdcee74e8fac905c5be31b583b45081
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-03 21:43:17 +0800

    Day02
    Perbaikan

--- Day02.java
+++ Day02.java
@@ -11,16 +11,16 @@ public class App {
 
         // 1. First atau pertama tama nih prtintln awalan buat baris baru 
         System.out.println("=== BIODATA DIRI ===");
-        System.out.println("nama : MuhammadFaiz");
-        System.out.println("status : MahaSiswa");
-        System.out.println("rank : MentokGlory");
-        System.out.println("statuss : Jomblohappy");
+        System.out.println("Nama : " + nama);
+        System.out.println("Status : " + status);
+        System.out.println("Rank : " + rank);
+        System.out.println("Statuss : " + statuss);
 
         // 2. Second atau dua atau two print untuk ndak ada lagi baris baru
-        System.out.print("umur : 19");
+        System.out.print("Umur : " + umur);
 
         // 3. Third atau ketiga terakhir jga bisa printf untuk mencetak format khusus 
-        System.out.printf(" tinggi : 166");
+        System.out.printf("%nTinggi : %.of cm%n" + tinggi);
 
 
     }
