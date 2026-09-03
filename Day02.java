575e52ba06cb3939a15fcb10f2cbbcd4450e0eaf
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-03 21:21:28 +0800

    Day02
    Cihuy

---
+++ Day02.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // How to make biodata diri menggunakan, use, memakai print, println dan printf
+
+        String nama = "MuhammadFaiz";
+        String status = "MahaSiswa";
+        String rank = "MentokGlory";
+        String statuss = "Jomblohappy";
+        int umur = 19;
+        double tinggi = 166;
+
+        // 1. First atau pertama tama nih prtintln awalan buat baris baru 
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("nama : MuhammadFaiz");
+        System.out.println("status : MahaSiswa");
+        System.out.println("rank : MentokGlory");
+        System.out.println("statuss : Jomblohappy");
+
+        // 2. Second atau dua atau two print untuk ndak ada lagi baris baru
+        System.out.print("umur : 19");
+
+        // 3. Third atau ketiga terakhir jga bisa printf untuk mencetak format khusus 
+        System.out.printf(" tinggi : 166");
+
+
+    }
+}
