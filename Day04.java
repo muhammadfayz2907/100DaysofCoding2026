58bcaf7213d43e999a8040545d5e7a1bdaa913e4
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-05 19:42:20 +0800

    Day04
    This Java program prints character stats after defeating a boss.

---
+++ Day04.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        // byte memiliki ukuran 8-bit, rentang dari -128 hingga 127.
+        // short memiliki ukuran 16-bit, rentang dari -32.768 hingga 32.767.
+        /* int memiliki ukuran 32-bit, rentang dari -2.147.483.648
+        hingga 2.147.483.647. */
+        /* long memiliki ukuran 64-bit, rentang dari -9.223.372.036.854.775.808
+        hingga 9.223.372.036.854.775.807. */
+        
+        byte naiklevel = 1;
+        short bataslevel = 999;
+        int mana = 999999;
+        long exp = 99999999L; 
+        // khusus untuk nilai LONG harus ditambahkan (L) dibelakang angka
+
+System.out.println("Yamada telah membunuh bos terakhir");
+System.out.println("---Stats---");
+System.out.println("NaikLevel: " + naiklevel);
+System.out.println("Level: " + bataslevel);
+System.out.println("Mana Sekarang: " + bataslevel);
+System.out.println("Exp yang di butuhkan untuk naik level: " + exp );
+System.out.println("Akhirnya saya sudah mengalahkan bos dan naik level");
+System.out.println("Waktunya saya kembali ke guild petualang untuk melaporkan nya");
+
+    }
+                   }
