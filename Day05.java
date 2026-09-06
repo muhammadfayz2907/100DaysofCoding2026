63cc0fbbff8969433fe0c2cc105f5801fb501d67
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-06 20:28:29 +0800

    Day05

---
+++ Day05.java
@@ -0,0 +1,16 @@
+//Day5menuju100DaysofCoding
+    public class Day5 {
+    public static void main(String[] args) {
+    //Deklarasi
+
+    //Berat badan menggunakan tipe data double
+        double berat;
+        float tinggi;
+        //inisialisasi
+        berat = 55.5;
+        tinggi = 160.25f;
+        //output
+        System.out.printf("%s: %.1f kg%n","berat badan", berat);
+        System.out.printf("%s: %.2f cm%n","tinggi badan", tinggi);
+        
+    }
