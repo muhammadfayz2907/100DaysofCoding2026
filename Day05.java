66d1bb3e94bcc2a8eb8477e1fc865754967bcfa2
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-06 20:53:26 +0800

    Day05
    Tugas pertama

--- Day05.java
+++ Day05.java
@@ -1,16 +1,14 @@
 //Day5menuju100DaysofCoding
     public class Day5 {
     public static void main(String[] args) {
-    //Deklarasi
+    //Tugas-01 : deklarasikan tinggi badan dan berat badan
 
-    //Berat badan menggunakan tipe data double
-        double berat;
-        float tinggi;
-        //inisialisasi
-        berat = 55.5;
-        tinggi = 160.25f;
-        //output
-        System.out.printf("%s: %.1f kg%n","berat badan", berat);
-        System.out.printf("%s: %.2f cm%n","tinggi badan", tinggi);
-        
+    //Berat badan menggunakan tipe data float
+        float bb = 55.5f;
+    //Tinggi badan menggunakan tipe data double
+        double tb = 160.25;
+
+        System.out.printf("Berat badan : %.1f kg%n", bb);
+        System.out.printf("Tinggi badan: %.2f cm%n", tb);
     }
+}
