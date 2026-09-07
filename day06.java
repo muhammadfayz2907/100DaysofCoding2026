74f9a03c11e563f3a4b3f0e7b3ae82a6122ebc7a
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-07 15:54:51 +0800

    Day06
    Cihuy

---
+++ day06.java
@@ -0,0 +1,20 @@
+public class day06 {
+    public static void main(String[] args) {
+    /* Muhammad Faiz adalah mahasiswa baru di kelas-D, yang berasal dari
+majene di galung utara yang baru belajar dalam bahasa percodingan, dan
+baru mengikuti organisasi isc :
+*/
+
+    String nama = "Muhammad Faiz";
+    int umur = 19;
+    double Ipk = 3.4;
+    char kodeJurusan = 'I';
+    boolean status = true;
+    System.out.println("Nama: \t" + nama);
+    System.out.println("Umur: \t" + umur);
+    System.out.println("IPK: \t" + Ipk);
+    System.out.println("KodeJurusan: \t" + kodeJurusan);
+    System.out.println("Status: \t" + status);
+    }
+
+}
