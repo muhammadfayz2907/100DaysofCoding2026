ed0e24c67e05d41deffc4aead4942ac6fe547f3d
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-08 19:57:20 +0800

    Day07
    Adegan Rimuru Tempest mendapatkan nama nya atau asal usul nama nya

---
+++ Day07.java
@@ -0,0 +1,29 @@
+public class day07 {
+    public static void main(String[] args) {
+    /*Percakapan episode dua Tensei shitara Slime datta ken season 1*/
+      
+      String teks1 = "Buatlah nama yang keren";
+      String teks2 = "Kamu juga";
+      String teks3 = "Naga badai,veldora";
+      String teks4 = "Bahasa inggris badai..";
+      String teks5 = "Storm?, agak aneh";
+      String teks6 = "TEMPEST";
+      String teks7 = "Bagaimana kalau Tempest";
+      String teks8 = "APAA, Tempest katamu?";
+      String teks9 = "Nama yang luar biasa!";
+
+      System.out.println("Veldora:\t" + teks1);
+      System.out.println("Slime:\t" + teks2);
+      System.out.println("Slime:\t" + teks3);
+      System.out.println("Slime:\t" + teks4);
+      System.out.println("Slime:\t" + teks5);
+      System.out.println("Slime:\t" + teks6);
+      System.out.println("Slime:\t" + teks7);
+      System.out.println("Veldora:\t" + teks8);
+      System.out.println("Veldora:\t" + teks9);
+
+
+
+
+    }
+}
