829868ede64593ff740af6d4d1d42f4a2caa721f
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-09 21:42:34 +0800

    Day08
    Cihuyyy

---
+++ Day08.java
@@ -0,0 +1,67 @@
+//Day08
+
+public class Main {
+    public static void main(String[] args) {
+
+     String Program = "Baiklah Tuan";
+     String Nama = "Rimuru";
+     String Nama_Player = "Rimuru Tempest";
+     String Rank = "Slime";
+     int Level = 1;
+     int Mana = 10;
+
+     System.out.println("Rimuru : AKU MASUK ISEKAI?");
+     System.out.println("Rimuru : Bagaimana cara melihat stats");
+     System.out.println("Rimuru : BUKA STATS");
+     System.out.println("Sistem : " + Program);
+     System.out.println("Sistem : MEMBUKA STATS");
+
+     System.out.println("=======================");
+     System.out.println("|    STATUS PLAYER    |");
+     System.out.println("=======================");
+     System.out.println("NAMA  : " + Nama_Player);
+     System.out.println("RANK  : " + Rank);
+     System.out.println("LEVEL :" + Level);
+     System.out.println("MANA  :" + Mana);
+     System.out.println("=======================");
+     System.out.println("|    STATUS PLAYER    |");
+     System.out.println("=======================");
+
+     System.out.println("Rimuru : Aku harus menjadi kuat");
+     System.out.println("Saat itu Rimuru mulai menaikan levelnya");
+
+     System.out.println("!LEVEL UP!");
+     System.out.println("!LEVEL UP! !LEVEL UP!");
+     System.out.println("!LEVEL UP!");
+     System.out.println("!LEVEL UP! !LEVEL UP!");
+     System.out.println("!LEVEL UP!");
+     System.out.println("!LEVEL UP!");
+
+     //STATS YANG TERBARU
+
+     String RAnk = "RAJA IBLIS";
+     int LEvel = 99;
+     int MAna = 99999;
+
+     //STATS YANG TERBARU
+
+     System.out.println("!Membuka Rank Baru : RAJA IBLIS!");
+     System.out.println("Rimuru : Akhirnya udah 2 tahun semenjak aku masuk isekai");
+     System.out.println("Rimuru : Aku sudah menjadi RAJA IBLISSS");
+     System.out.println("Rimuru : BWAHAHAWAHA");
+     System.out.println("Rimuru : Coba aku lihat stats dulu");
+     System.out.println("Rimuru : SISTEM BUKA STATS");
+
+     System.out.println("=======================");
+     System.out.println("|    STATUS PLAYER    |");
+     System.out.println("=======================");
+     System.out.println("NAMA  : " + Nama_Player);
+     System.out.println("RANK  : " + RAnk);
+     System.out.println("LEVEL :" + LEvel);
+     System.out.println("MANA  :" + MAna);
+     System.out.println("=======================");
+     System.out.println("|    STATUS PLAYER    |");
+     System.out.println("=======================");
+
+    }
+}
