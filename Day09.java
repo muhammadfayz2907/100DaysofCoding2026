5603663f13a0aa79331efefb6a4bb1a96dc526c3
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-10 20:18:20 +0800

    Day09
    Rimuru Tempest

---
+++ Day09.java
@@ -0,0 +1,19 @@
+//Day09
+//Rimuru Tempest
+
+public class Day9{
+
+    public static void main(String[] args) {
+
+      final String RajaIblis = "Rimuru Tempest";
+      final int Power = 999;
+      final boolean Op = true;
+
+      System.out.println("==========================");
+      System.out.println("RajaIblis : " + RajaIblis);
+      System.out.println("Power     : " + Power);
+      System.out.println("Op        :" + Op);
+      System.out.println("==========================");
+      System.out.println("Rimuru    : BWHWHWHWHWHWH");
+    }
+}
