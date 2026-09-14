05073063c6696e5a9f3913e881f2e0bb01c3f593
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-14 22:32:40 +0800

    Rename Main class to Day13

---
+++ Day13.java
@@ -0,0 +1,24 @@
+public class Day13 {
+    public static void main(String[] args) {
+      String nama = "Jefri";
+      double a = 2321;
+      float b = 423234f;
+      int c = 29238;
+      short d = 37;
+      long e = 2732632L;
+      boolean g = true;
+      char h = 'a';
+      byte f = 53;
+
+      System.out.println(nama);
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(e);
+      System.out.println(f);
+      System.out.println(g);
+      System.out.println(h);
+
+    }
+}
