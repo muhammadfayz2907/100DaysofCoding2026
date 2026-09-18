b3d61ee7d009cb1b1436026789995dd7c37761b9
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-18 22:59:53 +0800

    Add basic arithmetic operations in Day17.java
    cihuy

---
+++ Day17.java
@@ -0,0 +1,49 @@
+public class Day17{
+
+  //Push rank no 1
+    
+    public static void main(String[] args) {
+       
+        // untuk memberi nilai
+      
+        int A = 70; 
+
+        // Untuk menambah
+      
+        int B = 70;
+        B += 70;        
+
+        // Untuk mengurang
+
+        int C = 70;
+        C -= 20;        
+
+        // Untuk mengali
+
+      
+        int D = 70;
+        D *= 2;        
+
+        // Untuk membagi
+      
+        int E = 70;
+        E /= 7;       
+
+        // Untuk membagi sisa hasil pembagian
+      
+        int F = 70;
+        F %= 10;        
+
+        System.out.println("Nilai:" + A);
+        System.out.println("Hasil tambah:" + B);
+        System.out.println("Hasil kurang:" + C);
+        System.out.println("Hasil kali an:" + D);
+        System.out.println("Hasil pembagi:" +);
+        System.out.println("Hasil sisa hasil pembagi:" + F);
+        
+        
+        
+        
+    }
+  
+    }
