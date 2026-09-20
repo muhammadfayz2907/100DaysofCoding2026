743e188945c899f9def53167a0e4e1d9fafb5e93
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-20 21:37:34 +0800

    Add Day19.java for type casting demonstration

---
+++ Day19.java
@@ -0,0 +1,31 @@
+public class Day19 {
+    public static void main(String[] args) {
+        
+        double nilaiDouble = 67.67;
+
+//nilaifloat
+
+        float nilaiFloat = (float) nilaiDouble;
+
+//nilailong
+
+        long nilaiLong = (long) nilaiFloat;
+
+//nilaiint
+
+        int nilaiInt = (int) nilaiLong;
+
+//nilaiehort
+
+        short nilaiShort = (short) nilaiInt;
+
+//nilaibyte
+
+        byte nilaiByte = (byte) nilaiShort;
+
+        System.out.printf("NILAI DOUBLE\t:%f\n NILAI FLOAT\t:%f\n NILAI LONG\t\t:%d\n NILAI INT\t\t:%d\n NILAI SHORT\t:%d\n NILAI BYTE\t\t:%d\n", nilaiDouble, nilaiFloat, nilaiLong, nilaiInt, nilaiShort, nilaiByte);
+
+
+      
+    }
+}
