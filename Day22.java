49e058095bdfde739be377c8920ea61f443c4dc6
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-23 22:29:05 +0800

    Menukar angka

---
+++ Day22.java
@@ -0,0 +1,24 @@
+public class Day22 {
+    
+    public static void main(String[] args) {
+        
+        int A = 67;
+        int B = 969;
+        
+        System.out.println("=== HASIL NYA SEBELEM DITUKAR ===");
+        System.out.println("Nilai A\t\t: " + A);
+        System.out.println("Nilai B\t\t: " + B);
+        
+        
+        int simpan = A;
+        A = B;
+        B = simpan;
+        
+        System.out.println("=== HASIL NYA SESUDAH DITUKAR ===");
+        System.out.println("Nilai A\t\t: " + A + " Skylark");
+        System.out.println("Nilai B\t\t: " + B + " Six Seven");
+        
+        
+    }
+    
+}
