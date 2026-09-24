68b941aa3e878ef9ffd2fce8198487dbc8e82b40
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-24 20:35:46 +0800

    Bantu rimuru menghitung luas dari persegi

---
+++ Day 23.java
@@ -0,0 +1,17 @@
+public class Day23 {
+    public static void main(String[] args) {
+        System.out.println("Bantulah rimuru untuk mengerjakan soal");
+        System.out.println("=== SOAL ===");
+        System.out.println("1. Ada sebuah persegi dan sisi nya 18 cm, carilah luas dari persegi!");
+        
+        //isi nilai sisi dari persegi
+        int sisi = 18;
+        
+        int luas = sisi * sisi;
+        
+        System.out.println("sisi persegi = " + sisi + "cm");
+        System.out.println("luas persegi = " + luas + "cm");
+
+    }
+
+                           }
