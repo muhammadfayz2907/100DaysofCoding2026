36d52d41d6851d8b580f2d5640a16a3bb0d64c54
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-25 01:40:54 +0800

    Add Day24 class to menghitung persegi panjang

---
+++ Day24.java
@@ -0,0 +1,18 @@
+public class Day24 {
+    public static void main(String[] args) {
+      
+      System.out.println("Pada hari ini di adakan kuis");
+      System.out.println("Jadi kuis pada hari ini kita akan menghitung luas persegi panjang");
+      System.out.println("=== SOAL ===");
+      System.out.println("1. Budi memiliki sebuah bangunan persegi panjang yang dimana panjang dan lebarnya\n PANJANG \t:15\n LEBAR \t\t:20\nHitunglah Luas dari persegi panjang tersebut!");
+      System.out.println("");
+      //Nilai panjang nya:
+      int P = 15;
+      //Nilai lebar nya:
+      int L = 20;
+      //Hasil perkalian panjang dan lebar:
+      int Luas = P * L;
+
+      System.out.printf("Diketahui;\nPanjangnya\t:%d\nLebarnya\t:%d\n\nPenyelesaian Panjang x Lebar untuk mendapatkan luasnya\nHasilnya:\nLuasnya\t\t:%d", P, L, Luas);
+    }
+}
