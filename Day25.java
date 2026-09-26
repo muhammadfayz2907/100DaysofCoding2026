0446607a115ae720c3c59f37681fb84c549c5d3a
muhammadfayz2907 <muhfaizfaiz290@gmail.com>
2026-09-26 23:14:46 +0800

    Add Day25.java to calculate circle area

---
+++ Day25.java
@@ -0,0 +1,14 @@
+public class Day25 {
+    
+    public static void main(String[] args) {
+        
+        //jari jari lingkaran
+        double r = 15;
+        //mencari luas lingkaran dengan nilai pi dan jari jari nya
+        double luas = Math.PI * r * r;
+
+        System.out.printf("Luas lingkaran   :%.2f", luas);
+
+    }
+    
+}
